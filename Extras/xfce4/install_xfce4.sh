fa0f96cfa1672d70076084bd6e337d73b2bcfe26
KalyanKanna <kannakalyan05@gmail.com>
2025-05-29 16:35:28 +0530

    Last updated on 29/05/2025 16:35:28

--- Extras/xfce4/install_xfce4.sh
+++ Extras/xfce4/install_xfce4.sh
@@ -2,10 +2,10 @@
 
 # Core XFCE + LightDM + Greeter packages
 main_packages=(
+  xfce4
   lightdm
   lightdm-gtk-greeter
   lightdm-gtk-greeter-settings
-  xfce4
 )
 
 # Extra XFCE utilities and plugins
@@ -18,26 +18,14 @@ extra_packages=(
   xfce4-whiskermenu-plugin
 )
 
-# Function to install a package if it's not already installed
-install_software() {
-  if yay -Q "$1" &>/dev/null; then
-    echo -e "[\e[1;32mINFO\e[0m] $1 is already installed."
-  else
-    echo -e "[\e[1;34mINSTALL\e[0m] Installing $1..."
-    yay -S --needed "$1"
-  fi
-}
-
-# Start of script
 clear
 echo -e "[\e[1;33mINFO\e[0m] XFCE4 and Archive Tool Installer Script"
 
 # Install XFCE and LightDM
 read -rep $'\n[\e[1;33mACTION\e[0m] - Install XFCE4 and LightDM? (y/n): ' INST
 if [[ "$INST" =~ ^[Yy]$ ]]; then
-  for pkg in "${main_packages[@]}"; do
-    install_software "$pkg"
-  done
+  echo -e "[\e[1;34mINSTALL\e[0m] Installing XFCE4 and LightDM packages..."
+  yay -S --needed "${main_packages[@]}"
 
   echo -e "[\e[1;34mSYSTEMD\e[0m] Enabling LightDM..."
   sudo systemctl enable lightdm.service
@@ -46,12 +34,11 @@ fi
 # Install extra XFCE utilities
 read -rep $'\n[\e[1;33mACTION\e[0m] - Install extra XFCE utilities and plugins? (y/n): ' INST
 if [[ "$INST" =~ ^[Yy]$ ]]; then
-  for pkg in "${extra_packages[@]}"; do
-    install_software "$pkg"
-  done
+  echo -e "[\e[1;34mINSTALL\e[0m] Installing extra XFCE packages..."
+  yay -S --needed "${extra_packages[@]}"
 fi
 
-# Install archive tools from external script
+# Run archive tool installer script
 read -rep $'\n[\e[1;33mACTION\e[0m] - Install archive and compression tools (zip, tar, etc)? (y/n): ' INST
 if [[ "$INST" =~ ^[Yy]$ ]]; then
   ARCHIVER_SCRIPT="$(dirname "$0")/../install_archivers.sh"
